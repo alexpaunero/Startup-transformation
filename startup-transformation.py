2f871a2e1beb3bc235e3e0e1e46f9342fc6c0c8e
alexpaunero <103605819+alexpaunero@users.noreply.github.com>
2022-05-20 23:14:31 +0100

    Update startup-transformation.py

--- startup-transformation.py
+++ startup-transformation.py
@@ -8,6 +8,7 @@ import numpy as np
 # load in financial data
 financial_data = pd.read_csv('financial_data.csv')
 
+# code goes here
 #Task 1
 print(financial_data.head())
 
@@ -18,4 +19,23 @@ expenses = financial_data["Expenses"]
 
 #Task 3
 plt.plot(month, revenue)
+#plt.show()
+
+#Task 4
+plt.xlabel('Month')
+plt.ylabel('Amount ($)')
+plt.title('Revenue')
 plt.show()
+plt.clf()
+
+#Task 5
+plt.plot(month, expenses)
+plt.xlabel('Month')
+plt.ylabel('Amount ($)')
+plt.title('Expenses')
+plt.show()
+plt.clf()
+
+#Task 6
+expense_overview = pd.read_csv('expenses.csv')
+print(expense_overview.head())
